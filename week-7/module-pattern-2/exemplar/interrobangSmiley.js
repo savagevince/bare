92e766293836b65b4584aa994464f89929dc5b13
Sophie Gill <sophie@makersacademy.com>
2019-10-22 12:39:58 +0100

    moves solution into exemplar

--- week-7/module-pattern-2/exemplar/interrobangSmiley.js
+++ week-7/module-pattern-2/exemplar/interrobangSmiley.js
@@ -1,9 +1,8 @@
 "use strict";
 
 (function(exports) {
-  var QUESTION_MARK_COUNT = 2
   function interrobangSmiley(text) {
-    return text + "?".repeat(QUESTION_MARK_COUNT) + "!" + " :)"
+    return smiley(interrobang(text))
   }
   exports.interrobangSmiley = interrobangSmiley;
 })(this);
